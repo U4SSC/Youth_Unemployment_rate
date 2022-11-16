257ddf69bef2e9b8b085197128701354887807a4
Davinci <ericchen1201@alum.ccu.edu.tw>
2022-11-16 13:49:11 +0800

    install req

--- plot/generator.py
+++ plot/generator.py
@@ -1,6 +1,7 @@
 import os
 
 if __name__ == "__main__":
+    os.system("pip3 install -r requirements.txt")
     os.system("python create_scatter_plot.py")
     os.system("python create_flat_world_plot.py")
     os.system("python create_circle_world_plot.py")
